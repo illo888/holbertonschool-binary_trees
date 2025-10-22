f270e7125017e429be3c5eb58a9c01ae0ffcb95e
root <root@ip-10-42-244-4.ec2.internal>
2025-10-22 13:09:45 +0000

    added a Betty-style documentation comment for the function 11-binary_tree_size.c

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,5 +1,10 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_size - Measures the size of a binary tree
+ * @tree: Pointer to the root node of the tree to measure
+ *
+ * Return: Total number of nodes in the tree
+ */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (tree == NULL)
