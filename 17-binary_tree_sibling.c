efc2a9c19bc7c22a98e76b463f36dceaf05af230
root <root@ip-10-42-244-4.ec2.internal>
2025-10-22 13:15:35 +0000

    added a Betty-style documentation comment for the function 17-binary_tree_sibling.c

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,5 +1,10 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_nodes - Counts the nodes with at least one child in a binary tree
+ * @tree: Pointer to the root node of the tree to count
+ *
+ * Return: Number of non-leaf nodes (nodes with at least one child)
+ */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL)
