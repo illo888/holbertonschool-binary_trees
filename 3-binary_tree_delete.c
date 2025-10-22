667f4dbad813c7c2803729e8ecce13afa021484d
root <root@ip-10-42-244-4.ec2.internal>
2025-10-22 12:58:15 +0000

    You added a Betty-style documentation comment for the function 3-binary_tree_delete.c

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,5 +1,10 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_delete - Deletes an entire binary tree
+ * @tree: Pointer to the root node of the tree to delete
+ *
+ * Return: Nothing
+ */
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
